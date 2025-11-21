949c998c9a97ca9a4e95273d6817d157d6bbb36c
lukboy223 <ltvanekeren@gmail.com>
2025-11-21 17:01:26 +0100

    panda and bottom section responsive

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,21 @@
 
 <template>
   <header id="nav" class="w-full">
-    <nav class="md:w-1/2 w-full md:ml-[10%]">
+    <nav class="md:w-1/2 w-full md:ml-[10%] absolute">
       <ul class="flex justify-between text-white font-serif text-2xl m-5">
         <li class="relative">
-          <div
-            class="absolute bg-[#00B5A9] rotate-[-35deg] -z-10 w-[20em] h-[3em] top-[-426%] left-[-60%] translate-y-[-125%] translate-x-[27%] duration-500"
-            id="Home">
-          </div>
-          <router-link :to="{ name: 'Home' }" onmouseenter="navButtonAnimation('start', 'Home')"
-            onmouseleave="navButtonAnimation('leave', 'Home')">Home</router-link>
+          <router-link :to="{ name: 'Home' }">Home</router-link>
         </li>
         <li class="relative">
-          <div
-            class="absolute bg-[#00B5A9] rotate-[-35deg] -z-10 w-[20em] h-[3em] top-[-426%] left-[-43%] translate-y-[-125%] translate-x-[27%] duration-500"
-            id="Projects">
-          </div>
-          <router-link :to="{ name: 'Projects' }" onmouseenter="navButtonAnimation('start', 'Projects')"
-            onmouseleave="navButtonAnimation('leave', 'Projects')">Projects</router-link>
+          <router-link :to="{ name: 'Projects' }">Projects</router-link>
         </li>
         <li class="relative">
-          <div
-            class="absolute bg-[#00B5A9] rotate-[-35deg] -z-10 w-[20em] h-[3em] top-[-426%] left-[-60%] translate-y-[-125%] translate-x-[27%] duration-500"
-            id="About">
-          </div>
-          <router-link :to="{ name: 'About' }" onmouseenter="navButtonAnimation('start', 'About')"
-            onmouseleave="navButtonAnimation('leave', 'About')">About me</router-link>
+          <router-link :to="{ name: 'About' }">About me</router-link>
         </li>
       </ul>
     </nav>
   </header>
-  <main id="app" class="relative mt-40">
+  <main id="app" class="relative">
     <router-view />
   </main>
 </template>
