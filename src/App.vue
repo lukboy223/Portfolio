6fb2f559665567a47a7c5607b7976d9f9810d9a8
lukboy223 <ltvanekeren@gmail.com>
2025-11-24 16:32:22 +0100

    homepage done

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,28 @@
   <header id="nav" class="w-full">
     <nav class="md:w-1/2 w-full md:ml-[10%] absolute">
       <ul class="flex justify-between text-white font-serif text-2xl m-5">
-        <li class="relative">
-          <router-link :to="{ name: 'Home' }">Home</router-link>
+        <li class="relative z-10">
+          <router-link :to="{ name: 'Home' }" class="">Home</router-link>
         </li>
-        <li class="relative">
+        <li class="relative z-10">
           <router-link :to="{ name: 'Projects' }">Projects</router-link>
         </li>
-        <li class="relative">
+        <li class="relative z-10">
           <router-link :to="{ name: 'About' }">About me</router-link>
         </li>
       </ul>
     </nav>
   </header>
   <main id="app" class="relative">
-    <router-view />
+    <div class="4xl:hidden">
+      <router-view />
+    </div>
+    <div class="hidden 4xl:flex">
+      <h1 class="relative serif text-white text-6xl m-auto w-2/3 top-[10em]">Use a smaller screen brother, why are you
+        using
+        this big
+        of a screen. It was already hard enough to make this website work on small screens. Go ahead zoom in</h1>
+    </div>
   </main>
 </template>
 
