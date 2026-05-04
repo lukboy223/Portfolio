0ad299d6ca5613b0d6e3b681c338adb82898e14c
lukboy223 <ltvanekeren@gmail.com>
2026-05-04 10:50:02 +0200

    email upadte

--- src/components/ContactFooter.vue
+++ src/components/ContactFooter.vue
@@ -20,21 +20,19 @@ const isLg = props.breakpoint === 'lg';
 
 <template>
     <div>
-        <h3 class="serif text-white text-5xl mb-20"
-            :class="isLg ? 'lg:ml-[7em]' : 'md:ml-[7em]'">Contact</h3>
+        <h3 class="serif text-white text-5xl mb-20" :class="isLg ? 'lg:ml-[7em]' : 'md:ml-[7em]'">Contact</h3>
         <ul class="text-white text-2xl sans">
-            <li class="mb-15 w-fit"
-                :class="isLg ? 'lg:ml-[10em]' : 'md:ml-[10em]'"
-                v-on:mouseover="showEmail('on')"
+            <li class="mb-15 w-fit" :class="isLg ? 'lg:ml-[10em]' : 'md:ml-[10em]'" v-on:mouseover="showEmail('on')"
                 v-on:mouseleave="showEmail('off')">
-                <a href="mailto:ltvanekeren@gmail.com" class="underline">E-mail</a>
-                <div class="text-base absolute duration-200" :class="email">ltvanekeren@gmail.com</div>
+                <a href="mailto:contact@ltve.eu" class="underline">E-mail</a>
+                <div class="text-base absolute duration-200" :class="email">contact@ltve.eu</div>
             </li>
             <li class="mb-15" :class="isLg ? 'lg:ml-[5em]' : 'md:ml-[5em]'">
-                <a href="https://www.linkedin.com/in/luka-van-ekeren-b270a3321/" target="_blank" class="underline">Linkedin</a>
+                <a href="https://www.linkedin.com/in/luka-van-ekeren-b270a3321/" target="_blank"
+                    class="underline">Linkedin</a>
             </li>
             <li>
-                <a href="https://github.com/lukboy223" target="_blank" class="underline">Github</a>
+                <a href="https://github.com/lukboy23" target="_blank" class="underline">Github</a>
             </li>
         </ul>
     </div>
